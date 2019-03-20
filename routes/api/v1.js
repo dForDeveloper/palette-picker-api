42b023fd18201eb244b2bcecf5c819939f81a37d
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-19 22:21:16 -0600

    Add get and post endpoints for projects

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -2,4 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/queries');
 
+router.get('/projects?', db.getProjectByQuery);
+router.get('/projects', db.getProjects);
+router.post('/projects', db.postToProjects);
+router.post('/palettes', db.postToPalettes);
+
 module.exports = router;
