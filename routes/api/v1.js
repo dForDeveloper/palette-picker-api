623081b56109f9740f19c20ac8642cce65799e10
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-24 16:20:04 -0600

    Add patch and delete endpoints

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/queries');
 
-router.get('/projects?', db.getProjectByQuery);
+router.get('/projects?', db.getProjects);
 router.get('/projects/:id/palettes', db.getPalettes);
 router.post('/projects', db.postToProjects);
 router.post('/palettes', db.postToPalettes);
+router.patch('/projects/:id', db.patchProject);
+router.patch('/palettes/:id', db.patchPalette);
+router.delete('/projects/:id', db.deleteProject);
+router.delete('/palettes/:id', db.deletePalette);
 
 module.exports = router;
