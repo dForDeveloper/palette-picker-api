ec6ade787e0cb94310b911212f1addaf9b2fef87
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-24 16:20:16 -0600

    Refactor get and post endpoints

--- db/queries.js
+++ db/queries.js
@@ -2,32 +2,118 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const db = require('knex')(configuration);
 
-const getProjectByQuery = async (req, res) => {
-  const projects = await db('projects').where(req.query);
-  if (projects.length > 0) return res.status(200).json(projects);
-  res.sendStatus(404);
+const getProjects = async (req, res) => {
+  try {
+    for (param in req.query) {
+      if (param !== 'name') {
+        return res.status(422).json(
+          `'name' is the only valid query parameter. ${param} is invalid.`
+        );
+      }
+    }
+    let projects;
+    if (req.query.name) {
+      projects = await db('projects').where(req.query);
+    } else {
+      projects = await db('projects').select();
+    }
+    if (projects.length === 0) return res.sendStatus(404);
+    res.status(200).json(projects);
+  } catch {
+    res.sendStatus(500);
+  }
 }
 
 const getPalettes = async (req, res) => {
-  const { id: project_id } = req.params;
-  const palettes = await db('palettes').where({ project_id });
-  res.status(200).json(palettes);
+  try {
+    const { id: project_id } = req.params;
+    const palettes = await db('palettes').where({ project_id });
+    if (palettes.length == 0) return res.sendStatus(404);
+    res.status(200).json(palettes);
+  } catch {
+    res.sendStatus(500);
+  }
 }
 
 const postToProjects = async (req, res) => {
-  const [id] = await db('projects').insert(req.body, 'id');
-  res.status(201).json({ id });
+  try {
+    if (!req.body.name) return res.status(422).json('name missing from body');
+    const [id] = await db('projects').insert(req.body, 'id');
+    res.status(201).json({ id });
+  } catch {
+    res.sendStatus(500);
+  }
 }
 
 const postToPalettes = async (req, res) => {
-  const palette = req.body;
-  const [id] = await db('palettes').insert(palette, 'id');
-  res.status(201).json({ id });
+  try {
+    const palette = req.body;
+    const requiredParams = [
+      'name',
+      'color1',
+      'color2',
+      'color3',
+      'color4',
+      'color5',
+      'project_id'
+    ];
+    for (let param of requiredParams) {
+      if (!palette[param]) {
+        return res.status(422).json(
+          `Parameters must be exactly ${requiredParams.join(' ')}.`
+        );
+      }
+    }
+    const [id] = await db('palettes').insert(palette, 'id');
+    res.status(201).json({ id });
+  } catch {
+    res.sendStatus(500);
+  }
+}
+
+const patchProject = async (req, res) => {
+  try {
+    const { id, editedName } = req.body;
+    await db('projects').where({ id }).update({ name: editedName });
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(500);    
+  }
+}
+
+const patchPalette = async (req, res) => {
+  try {
+    const { id, editedName } = req.body;
+    await db('palettes').where({ id }).update({ name: editedName });
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(500);    
+  }
+}
+
+const deleteProject = async (req, res) => {
+  try {
+    res.sendStatus(200).json('placeholder response')
+  } catch {
+    res.sendStatus(500);    
+  }
+}
+
+const deletePalette = async (req, res) => {
+  try {
+    res.sendStatus(200).json('placeholder response')
+  } catch {
+    res.sendStatus(500);    
+  }
 }
 
 module.exports = {
-  getProjectByQuery,
+  getProjects,
   getPalettes,
   postToProjects,
-  postToPalettes
+  postToPalettes,
+  patchProject,
+  patchPalette,
+  deleteProject,
+  deletePalette
 }
