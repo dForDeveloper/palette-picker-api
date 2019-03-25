39cb141a1d103132cb540e99f557127fd84dca00
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-25 11:50:05 -0600

    Add test configuration to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,21 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/palettepicker',
+    connection: 'postgres://localhost/palette_picker',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette_picker_test',
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds/test'
     },
     useNullAsDefault: true
   }
